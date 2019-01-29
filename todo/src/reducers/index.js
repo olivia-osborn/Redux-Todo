efd6d014d9942e3bfc2b03a18515a4f436edccdf
Olivia Osborn <olioz95@gmail.com>
2019-01-29 15:15:25 -0500

    added reducer function

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,3 +1,20 @@
 const initialState = {
-    todoList: ["Clean room"]
+    todoList: [
+    {
+        value: "Walk the dog",
+        completed: false
+    }
+    ]
+}
+
+function reducer(state= initialState, action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todoList: [...state.todoList, newTodo]
+            }
+        default:
+            return state;
+    }
 }
